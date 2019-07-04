d251181e0a39a40b97f2e66d5c9e70962ca0a1cd
Focus5679 <35841406+Focus5679@users.noreply.github.com>
2019-07-04 17:21:22 +0800

    Update pipelines.py

--- getzhipin/getzhipin/pipelines.py
+++ getzhipin/getzhipin/pipelines.py
@@ -17,7 +17,7 @@ class GetzhipinJobInfoPipeline(object):
         self.conn = pymysql.connect(host='127.0.0.1',
                                     port=3306,
                                     user='root',
-                                    password='12345678',
+                                    password='********',
                                     db='zhipinjob',
                                     charset='utf8')
         self.cursor = self.conn.cursor()
